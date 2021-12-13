707b68b0fc5f4d6c43fc9af56df8efd981d30809
adammathewvaughn <adammathewvaughn@gmail.com>
2021-12-13 16:28:27 -0600

    fixed a bunch of typos

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,9 @@ function App() {
         <h4>—Automating a better tomorrow—</h4>
         <p> &nbsp;&nbsp;I'm Adam M. Vaughn and I'm an Embedded Systems engineer with an emphasis on incorporating Full-Stack Development and REST APIs into microcontrolled systems. 
         My goal as an embedded systems engineer is to combine my recent Full-Stack Web-Development education with my background in construction and my love for 
-        hardware.As the juxtaposition of hardware and software has never been more at the forefront of our lives with the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino.
-        Embedded systems is the natural arris of the adjacent technological disciplines, and I'm excited to begin my path in the field.
-        I'm grateful for the opportunity to contribute to a better world through embedded systems and home automation, and I'm eagerly working to do more every day. 
-        I would love to talk to like-minded people, companies, organizations who are interested in achieving similar goals.</p>
+        hardware. With the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino, the juxtaposition of hardware and software has never been more at the forefront of our lives.
+        Embedded systems is the natural arris of the adjacent technological disciplines and I'm excited to begin my path in the field and I'm eagerly working to do more every day. I'm grateful for the opportunity to contribute to a better world through coding and embedded systems,
+        and I would love to speak with like-minded people, companies, organizations who are interested in achieving similar goals.</p>
           <div className="LinksDiv">
           <a
             className="App-link"
