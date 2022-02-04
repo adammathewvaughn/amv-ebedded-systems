68f71e42a414bf44ec4c8c301dd2eefadffb8cf0
adammathewvaughn <adammathewvaughn@gmail.com>
2022-02-03 23:41:35 -0600

    changed slogan to h1 and added icons to footer

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <div className="App-body col-md-6">
 
 
-            <h4 className="slogan col-md-6">—— Automating a better tomorrow ——</h4>
+            <h1 className="slogan col-md-6">—— Automating a better tomorrow ——</h1>
             <p> &nbsp; &nbsp; Hi, my name is Adam M. Vaughn and I'm a Birmingham-area Embedded Systems engineer with an emphasis on incorporating Full-Stack Development
               and REST APIs into microcontrolled systems. My goal as an embedded systems engineer is to combine my Full-Stack Web-Development education with my background in construction and my life-long infatuation with technology
               and hardware. With the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino, the juxtaposition of hardware and software has never been more at the forefront of our lives.
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome/'
 import "./Footer.css"
+import { faDiscord, faGithub, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 
+const i = <FontAwesomeIcon icon={faDiscord} />
+const j = <FontAwesomeIcon icon={faTwitterSquare} />
+const k = <FontAwesomeIcon icon={faGithub} />
 let year = new Date().getFullYear();
 const Footer = () => {
 return (
@@ -9,7 +14,7 @@ return (
         Made With <a className="IBLove" href="https://innovatebham.com" target="_blank" rel="noopener noreferrer">Love</a> by<a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank" rel="noopener noreferrer">Adam Mathew Vaughn</a>Embedded Systems | Birmingham, AL &copy; {year}
     </p>
     &nbsp;
-    <a href="https://github.com/adammathewvaughn" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://twitter.com/adammathewv" target="_blank" rel="noopener noreferrer">Twitter</a>| <a href="https://discord.gg/QDX6SpZe" target="_blank" rel="noopener noreferrer">Discord</a>
+    <a href="https://github.com/adammathewvaughn" target="_blank" rel="noopener noreferrer"> <i className="fas fa-github">{k}</i></a> | <a href="https://twitter.com/adammathewv" target="_blank" rel="noopener noreferrer"> <i className="fas fa-twittersquare"> {j}</i></a>| <a href="https://discord.gg/QDX6SpZe" target="_blank" rel="noopener noreferrer"> <i className="fas fa-discord"> {i}</i></a>
     </footer>
 )
 };
