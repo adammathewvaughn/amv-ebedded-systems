634d6bc55dad4366343ecd79bd907e2fc2122e33
adammathewvaughn <adammathewvaughn@gmail.com>
2022-02-04 23:19:02 -0600

    adjusted css made a div for content

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import './App.css'
 import TagManager from 'react-gtm-module'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome/'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { faRaspberryPi } from '@fortawesome/free-brands-svg-icons';
 
 
 
 
 const i = <FontAwesomeIcon icon={faCoffee} />
+// const j = <FontAwesomeIcon icon={faRaspberryPi} /> <i className="fa-brands fa-raspberry-pi">{j}</i>
 
 const tagManagerArgs = {
   gtmId: 'GTM-MW7CTZK'
@@ -22,17 +24,18 @@ function App() {
     <div className="container fluid">
       <div className="App col-md-6">
         <Header className="col-md-6" />
+        <div className="container-fluid">
         <div className="col-md-6">
-          <div className="App-body col-md-6">
-
-
+        <div className="App-body col-md-6">
+        <div className="row"> 
+        <div className="content-left">
             <h1 className="slogan col-md-6">—— Automating a better tomorrow ——</h1>
             <p> &nbsp; &nbsp; Hi, my name is Adam M. Vaughn and I'm a Birmingham-area Embedded Systems engineer with an emphasis on incorporating Full-Stack Development
               and REST APIs into microcontrolled systems. My goal as an embedded systems engineer is to combine my Full-Stack Web-Development education with my background in construction and my life-long infatuation with technology
               and hardware. With the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino, the juxtaposition of hardware and software has never been more at the forefront of our lives.
               Embedded systems is the natural arris of the myriad technologies that comprise our modern world, and I'm excited to begin my path in the field. I'm grateful for the opportunity to contribute to a
               better world through coding and embedded systems, and I'm eagerly working to do more every day.
-              I would love to speak with like-minded people, companies, and organizations who are interested in achieving similar goals, and I hope we can work together soon.</p>
+              I would love to speak with like-minded people, companies, and organizations who are interested in achieving similar goals, and I hope we can work together soon.</p>  
             <div className="LinksDiv">
               <a
                 className="App-link"
@@ -63,9 +66,12 @@ function App() {
                 rel="noopener noreferrer"
               >Call
               </a>
+              </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
       </div>
       <Footer className="col-md-6" />
 
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
 return (
     <footer className="adam-foot">
     <p>
-        Made With <a className="IBLove" href="https://innovatebham.com" target="_blank" rel="noopener noreferrer">Love</a> by<a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank" rel="noopener noreferrer">Adam Mathew Vaughn</a>Embedded Systems | Birmingham, AL &copy; {year}
+        Made With <a className="IBLove" href="https://innovatebham.com" target="_blank" rel="noopener noreferrer">Love</a> by<a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank" rel="noopener noreferrer"><span className="brandName">Adam Mathew Vaughn</span></a>Embedded Systems | Birmingham, AL &copy; {year}
     </p>
     &nbsp;
-    <a href="https://github.com/adammathewvaughn" target="_blank" rel="noopener noreferrer"> <i className="fas fa-github">{k}</i></a> | <a href="https://twitter.com/adammathewv" target="_blank" rel="noopener noreferrer"> <i className="fas fa-twittersquare"> {j}</i></a>| <a href="https://discord.gg/QDX6SpZe" target="_blank" rel="noopener noreferrer"> <i className="fas fa-discord"> {i}</i></a>
+    <span className="footer-icons"><a href="https://github.com/adammathewvaughn" target="_blank" rel="noopener noreferrer"> <i className="fas fa-github">{k}</i></a> <a href="https://twitter.com/adammathewv" target="_blank" rel="noopener noreferrer"> <i className="fas fa-twittersquare">{j}</i></a> <a href="https://discord.gg/QDX6SpZe" target="_blank" rel="noopener noreferrer"> <i className="fas fa-discord">{i}</i></a></span>
     </footer>
 )
 };
