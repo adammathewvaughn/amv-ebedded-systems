2353a674f31e58a2e5859008760bc11b32634f35
adammathewvaughn <adammathewvaughn@gmail.com>
2022-02-05 23:03:23 -0600

    got pi icon imported, couldn't get it situated so commented it out for future reference

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 const i = <FontAwesomeIcon icon={faCoffee} />
-// const j = <FontAwesomeIcon icon={faRaspberryPi} /> <i className="fa-brands fa-raspberry-pi">{j}</i>
-
+// const j = <FontAwesomeIcon icon={faRaspberryPi} /> <i className="fa-brands fa-raspberry-pi mt-3 mb-0">{j}{j}{j}</i>
 const tagManagerArgs = {
   gtmId: 'GTM-MW7CTZK'
 }
@@ -22,21 +21,22 @@ function App() {
   const [count, setCount] = useState(0)
   return <>
     <div className="container fluid">
-      <div className="App col-md-6">
+      <div className="App row">
         <Header className="col-md-6" />
+        <div className="row-md-6">
         <div className="container-fluid">
-        <div className="col-md-6">
-        <div className="App-body col-md-6">
-        <div className="row"> 
-        <div className="content-left">
-            <h1 className="slogan col-md-6">—— Automating a better tomorrow ——</h1>
-            <p> &nbsp; &nbsp; Hi, my name is Adam M. Vaughn and I'm a Birmingham-area Embedded Systems engineer with an emphasis on incorporating Full-Stack Development
-              and REST APIs into microcontrolled systems. My goal as an embedded systems engineer is to combine my Full-Stack Web-Development education with my background in construction and my life-long infatuation with technology
-              and hardware. With the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino, the juxtaposition of hardware and software has never been more at the forefront of our lives.
-              Embedded systems is the natural arris of the myriad technologies that comprise our modern world, and I'm excited to begin my path in the field. I'm grateful for the opportunity to contribute to a
-              better world through coding and embedded systems, and I'm eagerly working to do more every day.
-              I would love to speak with like-minded people, companies, and organizations who are interested in achieving similar goals, and I hope we can work together soon.</p>  
-            <div className="LinksDiv">
+        <div className="App-body col-4">
+        <div className="row justify-content-between"> 
+        <div className="content-left row md-4 d-flex ">
+        
+        <h1 className="slogan row-md-6">—— Automating a better tomorrow ——</h1>
+        <p> &nbsp; &nbsp; Hi, my name is Adam M. Vaughn and I'm a Birmingham-area Embedded Systems engineer with an emphasis on incorporating Full-Stack Development
+        and REST APIs into microcontrolled systems. My goal as an embedded systems engineer is to combine my Full-Stack Web-Development education with my background in construction and my life-long infatuation with technology
+        and hardware. With the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino, the juxtaposition of hardware and software has never been more at the forefront of our lives.
+        Embedded systems is the natural arris of the myriad technologies that comprise our modern world, and I'm excited to begin my path in the field. I'm grateful for the opportunity to contribute to a
+        better world through coding and embedded systems, and I'm eagerly working to do more every day.
+        I would love to speak with like-minded people, companies, and organizations who are interested in achieving similar goals, and I hope we can work together soon.</p>  
+        <div className="LinksDiv col-4 justify-content-between">
               <a
                 className="App-link"
                 href="https://www.linkedin.com/in/vaughn-embedded-systems"
@@ -73,7 +73,7 @@ function App() {
         </div>
       </div>
       </div>
-      <Footer className="col-md-6" />
+      <Footer className="row-md-6" />
 
     </div>
   </>;
