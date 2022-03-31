413de373712f6c0328af6b795fe201acadae95e5
adammathewvaughn <adammathewvaughn@gmail.com>
2022-03-30 22:37:06 -0500

    removed text shadow in the body

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,14 @@ function App() {
   const [count, setCount] = useState(0)
   return <>
     <div className="container fluid">
-      <div className="App row">
+      <div className="App justify-content-center row md-4 d-flex ">
         <Header className="col-md-6" />
         <div className="row-md-6">
         <div className="container-fluid">
         <div className="App-body col-4">
         <div className="row justify-content-between"> 
         <div className="justify-content-left row md-4 d-flex ">
-        
+
         <h1 className="slogan row-md-6">—— Automating a better tomorrow ——</h1>
         <p className="bodyp"> &nbsp; &nbsp; Hi, my name is Adam M. Vaughn and I'm a Birmingham-area Embedded Systems engineer with an emphasis on incorporating Full-Stack Development
         and REST APIs into microcontrolled systems. My goal as an embedded systems engineer is to combine my Full-Stack Web-Development education with my background in construction and my life-long infatuation with technology
