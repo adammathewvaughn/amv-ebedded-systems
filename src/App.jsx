0e91a4372815d6207efded7aab55dd7666948434
adammathewvaughn <adammathewvaughn@gmail.com>
2022-04-24 23:08:11 -0500

    trying to fix style issues on mobile, leaning towards bootstrap to resolve

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,14 @@ function App() {
   return <>
     <div className="container fluid">
       <div className="App justify-content-center row md-4 d-flex ">
-        <Header className="col-md-6" />
-        <div className="row-md-6">
+        <Header className="col-sm-6" />
+        <div className="row-sm-6">
         <div className="container-fluid">
-        <div className="App-body col-4">
+        <div className="App-body col-sm-4">
         <div className="row justify-content-between"> 
-        <div className="justify-content-left row md-4 d-flex ">
+        <div className="justify-content-left row-sm-4 d-flex ">
 
-        <h1 className="slogan row-md-6">—— Automating a better tomorrow ——</h1>
+        <h1 className="slogan row-sm-6">—— Automating a better tomorrow ——</h1>
         <p className="bodyp"> &nbsp; &nbsp; Hi, my name is Adam M. Vaughn and I'm a Birmingham-area Embedded Systems engineer with an emphasis on incorporating Full-Stack Development
         and REST APIs into microcontrolled systems. My goal as an embedded systems engineer is to combine my Full-Stack Web-Development education with my background in construction and my life-long infatuation with technology
         and hardware. With the prevalence of the IoT and micro-controlled/automated embedded systems via Raspberry Pi and Arduino, the juxtaposition of hardware and software has never been more at the forefront of our lives.
@@ -73,7 +73,7 @@ function App() {
         </div>
       </div>
       </div>
-      <Footer className="row-md-4 d-flex" />
+      <Footer className="row-sm-4 d-flex" />
 
     </div>
   </>;
