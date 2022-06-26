1d40c4a1564deea535a0fb5a67c20098ffe62668
adammathewvaughn <adammathewvaughn@gmail.com>
2022-06-25 22:10:04 -0500

    preparing to paginate

---
+++ client/server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const apiRouter = require('./routes');
+let app = express();
+
+app.use(express.json());
+app.use(express.static('client'));
+app.use('/api', apiRouter());
+
+app.listen(3000);
